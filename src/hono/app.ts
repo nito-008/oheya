34376c4bc607a900b206b2f0b3209d69fb16685b
nito-008 <100199931+nito-008@users.noreply.github.com>
2026-04-20 17:37:46 +0900

    fix: middleware type

--- src/hono/app.ts
+++ src/hono/app.ts
@@ -1,7 +1,13 @@
 import { Hono } from "hono";
+import type { ApplyGlobalResponse } from "hono/client";
 import { usersRouter } from "./routes/users";
 import type { Bindings } from "./types";
 
 export const app = new Hono<{ Bindings: Bindings }>().basePath("/api").route("/users", usersRouter);
 
-export type AppType = typeof app;
+export type AppType = ApplyGlobalResponse<
+  typeof app,
+  {
+    401: { json: { message: string } };
+  }
+>;
