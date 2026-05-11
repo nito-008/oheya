afd278355851ddf1276bba5d51ded595cface6de
にと <100199931+nito-008@users.noreply.github.com>
2026-05-11 17:04:39 +0900

    feat: limit image uploads
    画像単体とユーザーごとのアップロード容量制限を追加。

--- drizzle/0000_sad_kronos.sql
+++ drizzle/0000_sad_kronos.sql
@@ -17,6 +17,7 @@ CREATE TABLE `account` (
 CREATE TABLE `image` (
 	`id` text PRIMARY KEY NOT NULL,
 	`user_id` text NOT NULL,
+	`byte_size` integer NOT NULL,
 	`created_at` integer NOT NULL,
 	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade
 );
